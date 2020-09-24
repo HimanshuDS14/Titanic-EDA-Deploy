a9630ea5d2caf03ec6f7a34665479007250b5542
HimanshuDS14 <56062778+HimanshuDS14@users.noreply.github.com>
2020-09-24 20:44:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"himanshu.poo.02@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
